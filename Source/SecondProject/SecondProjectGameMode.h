24422c3a7eec2445da5e9f8f6266edc3c6e467ff
kimyuja <kiy102205@sookmyung.ac.kr>
2024-03-25 16:18:10 +0900

    [init] (파일이 너무 커서 나눠서 push)

---
+++ Source/SecondProject/SecondProjectGameMode.h
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SecondProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASecondProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+
+	ASecondProjectGameMode();
+};
+
+
+
