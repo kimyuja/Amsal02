8c93250ea480b7613bc661d85d090d00f7386ce4
KSW1999 <kso8242@naver.com>
2024-04-02 19:42:46 +0900

    [Update]AI 경계도 제작중

--- Source/SecondProject/Public/Enemy.h
+++ Source/SecondProject/Public/Enemy.h
@@ -21,15 +21,25 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="My Settings")
 	class USceneComponent* checkPoint2;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="My Settings")
+	class UTextRenderComponent* warningComp;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="My Settings")
 	float Life = 100.0f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="My Settings")
+<<<<<<< Updated upstream
 	float damage;
+=======
+	class UAIPerceptionComponent* aiperception;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="My Settings")
-	FText warningText;
+	float Life = 100.0f;
+>>>>>>> Stashed changes
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="My Settings")
+	FText warningText = FText::FromString(".");
 	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="My Settings")
 	FVector startLocation;
@@ -43,4 +53,18 @@ public:
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+<<<<<<< Updated upstream
+=======
+	UFUNCTION()
+	void BasicMoveCycle1();
+
+	UFUNCTION()
+	void BasicMoveCycle2();
+
+	UFUNCTION(BlueprintCallable)
+	void Damaged(float damage);
+
+	UFUNCTION()
+	void OnPerceptionPlayer(const TArray<AActor*>& UpdatedActors);
+>>>>>>> Stashed changes
 };
