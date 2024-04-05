03f00041a342c3bb350b97ee8f51cc60220d9476
KSW1999 <kso8242@naver.com>
2024-04-05 18:20:48 +0900

    [Update] AI가 플레이어를 죽일 수 있게 됨

--- Source/SecondProject/SecondProjectCharacter.h
+++ Source/SecondProject/SecondProjectCharacter.h
@@ -69,5 +69,6 @@ public:
 	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 	/** Returns FollowCamera subobject **/
 	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
 };
 
